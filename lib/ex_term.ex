3208b41540676ebb79f2192c88c48aca5e304963
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-01-03 01:19:52 -0600

    gets copying content out of the terminal working

--- lib/ex_term.ex
+++ lib/ex_term.ex
@@ -84,6 +84,76 @@ defmodule ExTerm do
       (() => {
         const terminal = document.getElementById("exterm-terminal");
         terminal.addEventListener("exterm:mounted", event => event.target.scroll(0, 5));
+
+        const rowCol = (node) => {
+          var id;
+          if (node.nodeType === 3) { id = node.parentNode.id } else { id = node.id }
+          [_, _, row, col] = id.split("-");
+          return [Number(row), Number(col)];
+        }
+
+        const fetchCols = (row, [row1, col1], [row2, col2]) => {
+          var col_start = 0;
+          var line = "";
+          var index = 1;
+          var buffered_spaces = 0;
+          if (row == row1) { index = col1; }
+
+          while (true) {
+            element = document.getElementById("exterm-cell-" + row + "-" + index);
+            // pull out if we're at the end of the line.
+            if (!element) break;
+
+            // if it's empty then don't add it in, just increment buffered spaces
+            var content = element.textContent.trim();
+
+            if (content === "") {
+              buffered_spaces += 1;
+            } else {
+              line += " ".repeat(buffered_spaces) + content;
+              buffered_spaces = 0;
+            }
+
+            // pull out if we're at the end.
+            if (row == row2 && index == col2) break;
+            index += 1;
+          }
+          return line + "\n"
+        }
+
+        const fetchRows = (coord1, coord2) => {
+          // first put rows in order.
+          var start, end;
+          if (coord1[0] < coord2[0]) {
+            start = coord1;
+            end = coord2;
+          } else if (coord1[0] == coord2[0]) {
+            if (coord1[1] < coord2[1]) {
+              start = coord1;
+              end = coord2;
+            } else {
+              start = coord2;
+              end = coord1;
+            }
+          } else {
+            start = coord2;
+            end = coord1;
+          }
+          var result = "";
+          for (let index = start[0]; index <= end[0]; index++) {
+            result += fetchCols(index, start, end);
+          }
+          return result;
+        }
+
+        const modifyClipboard = (event) => {
+          const selection = window.getSelection();
+          copied = fetchRows(rowCol(selection.anchorNode), rowCol(selection.focusNode));
+          event.clipboardData.setData("text/plain", copied);
+          event.preventDefault();
+        }
+
+        terminal.addEventListener("copy", modifyClipboard)
       })()
     </script>
     """
