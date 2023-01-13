47dfe9824af151ef1c5a81647f350f1926f41892
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-01-13 12:01:52 -0600

    fixes JS delivery which causes dom update error

--- lib/ex_term/console.ex
+++ lib/ex_term/console.ex
@@ -50,7 +50,7 @@ defmodule ExTerm.Console do
   def render(assigns) do
     ~H"""
     <div id="exterm-console" phx-update="append">
-      <Cell.render :for={cell <- @cells} cell={cell} cursor={@cursor} prompt={@prompt}/>
+      <Cell.render contenteditable="false" :for={cell <- @cells} cell={cell} cursor={@cursor} prompt={@prompt}/>
     </div>
     """
   end
